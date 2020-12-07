6d9b27794e09a0391d493e257950c45e78fc7744
Timo Reitz <godsboss@gmx.de>
2020-12-07 21:14:59 +0100

    Extract function for reading input.

--- day1/expenses.go
+++ day1/expenses.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
@@ -16,18 +13,9 @@ func main() {
 }
 
 func run() error {
-	rawInput, err := ioutil.ReadFile("input.txt")
+	lines, err := readInput("input.txt")
 	if err != nil {
 		return err
-	}
-	rawLines := strings.Split(string(rawInput), "\n")
-	lines := make([]int, len(rawLines)-1)
-	for i := range lines {
-		line, err := strconv.Atoi(rawLines[i])
-		if err != nil {
-			fmt.Printf("Error in line %d: %+v\n", i+1, err)
-		}
-		lines[i] = line
 	}
 
 	for i := range lines {
---
+++ day1/read_input.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func readInput(filename string) ([]int, error) {
+	rawInput, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	rawLines := strings.Split(string(rawInput), "\n")
+	lines := make([]int, len(rawLines)-1)
+	for i := range lines {
+		line, err := strconv.Atoi(rawLines[i])
+		if err != nil {
+			return nil, fmt.Errorf("error in line %d: %+v", i+1, err)
+		}
+		lines[i] = line
+	}
+	return lines, nil
+}
