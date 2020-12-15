e665630ab01a1ed6548acaea687e1ad3c478ab83
Timo Reitz <godsboss@gmx.de>
2020-12-15 12:38:10 +0100

    Implement part 1.

--- days/day2/passwords.go
+++ days/day2/passwords.go
@@ -1,6 +1,10 @@
 package day2
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	"github.com/GodsBoss/aoc2020/pkg/cli"
 )
 
@@ -21,10 +25,30 @@ func NewDay(inputFilepath string) cli.Day {
 }
 
 func (d *sday) Init() error {
+	rawInput, err := ioutil.ReadFile(d.inputFilepath)
+	if err != nil {
+		return err
+	}
+	rawLines := strings.Split(string(rawInput), "\n")
+	pwRecs := make([]*passwordRecord, len(rawLines)-1)
+	for i := range pwRecs {
+		pwRecs[i], err = passwordRecordFromLine(rawLines[i])
+		if err != nil {
+			return fmt.Errorf("error in line %d: %+v", i+1, err)
+		}
+	}
+	d.pwRecords = pwRecs
 	return nil
 }
 
 func (d *sday) Part1() error {
+	validPasswordCount := 0
+	for _, pwRec := range d.pwRecords {
+		if pwRec.isValid() {
+			validPasswordCount++
+		}
+	}
+	fmt.Printf("There are %d valid passwords.\n", validPasswordCount)
 	return nil
 }
 
